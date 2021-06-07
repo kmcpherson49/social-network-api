61fedd4e2d3710d711ab8590179b7db80b517416
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-06-06 20:18:34 -0700

    update user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,20 +13,53 @@ getAllUsers(req, res) {
   },
 
 //GET single user
+getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughtText',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(userData => res.json(userData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
 //CREATE a user
+createUser({ body }, res) {
+    User.create(body)
+      .then(userData => res.json(userData))
+      .catch(err => res.json(err));
+  },
 
 //UPDATE a user
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(userData => {
+        if (!userData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch(err => res.json(err));
+  },
 
-
-//DELEYE user
+//DELETE user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(userData => res.json(userData))
+      .catch(err => res.json(err));
+  },
 
 //ADD friend
 updateFriend(req, res) {
     User.findOneAndUpdate({ _id: req.params.id }, {$addToSet: {friends: req.params.friendId}}, { new: true, runValidators: true })
       .then(friendData => {
         if (!friendData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: 'No friend found with this id!' });
           return;
         }
         res.json(friendData);
@@ -35,5 +68,12 @@ updateFriend(req, res) {
   },
 
 //REMOVE freind
+deleteFriend(req, res) {
+    User.findOneAndDelete({ _id: params.id }, {$pull: {friends: req.params.friendId}})
+      .then(friendData => res.json(friendData))
+      .catch(err => res.json(err));
+  }
 
 };
+
+module.exports = userController;
